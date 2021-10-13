6a84aa14e5914e0aa0b6825b31244aa7ec2a3d3b
Igor Nerovniy <i.nerovnyi@sovtes.ua>
2021-10-13 10:27:03 +0300

    Updates

---
+++ precache-manifest.bd4ca0a875b94d21e422eb62502fdbc2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a26990564eb1732f70a74dd267a802",
+    "url": "/redux-have-to-use/index.html"
+  },
+  {
+    "revision": "37f5042be080025772f6",
+    "url": "/redux-have-to-use/static/css/main.d5bd348c.chunk.css"
+  },
+  {
+    "revision": "687f5ec6c2249f79b47d",
+    "url": "/redux-have-to-use/static/js/2.00116750.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/redux-have-to-use/static/js/2.00116750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f5042be080025772f6",
+    "url": "/redux-have-to-use/static/js/main.0272d2dd.chunk.js"
+  },
+  {
+    "revision": "a414462a9c0ac1b09841",
+    "url": "/redux-have-to-use/static/js/runtime-main.d553f930.js"
+  },
+  {
+    "revision": "295be9fb41e124fbc4dd1392cccaaf6d",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Black.295be9fb.ttf"
+  },
+  {
+    "revision": "368636f1b6e330a4806185cdf6bb44bc",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Bold.368636f1.ttf"
+  },
+  {
+    "revision": "a1196de3f5de864f812b83bae4229e14",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Heavy.a1196de3.ttf"
+  },
+  {
+    "revision": "eebf2894b069d9cb93a29dab12aa31f5",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Light.eebf2894.ttf"
+  },
+  {
+    "revision": "edd77035474a9b292aadb486aecb9aaa",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-LightItalic.edd77035.ttf"
+  },
+  {
+    "revision": "c5a8e1f150a5b6d6d37e0f95220ed5f6",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Medium.c5a8e1f1.ttf"
+  },
+  {
+    "revision": "6987bcc482500f459516dc0342836ee5",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Regular.6987bcc4.ttf"
+  },
+  {
+    "revision": "0538ddc92082d2ef2e7b375519825dac",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Semibold.0538ddc9.ttf"
+  },
+  {
+    "revision": "616c9904ca0af2343d27c93029f87165",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Thin.616c9904.ttf"
+  },
+  {
+    "revision": "2a153e7817bfab335240d9eb2ee9a01d",
+    "url": "/redux-have-to-use/static/media/SFProDisplay-Ultralight.2a153e78.ttf"
+  }
+]);
